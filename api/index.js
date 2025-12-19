0f2d6740422904bdf90f19a2eb680c31d9a80339
alien-homesick <alien.and.homesick@gmail.com>
2025-12-18 17:17:16 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 const { addonBuilder } = require("stremio-addon-sdk");
-const manifest = require("./manifest.json");
+const manifest = require("../manifest.json");
 
 const builder = new addonBuilder(manifest);
 
 builder.defineStreamHandler((args) => {
-    console.log("Stremio requested ID:", args.id); // This helps us debug in Vercel logs
-    
     const idParts = args.id.split(":");
     const imdbId = idParts[0];
     let streamUrl = "";
@@ -13,7 +11,6 @@ builder.defineStreamHandler((args) => {
     if (args.type === "movie") {
         streamUrl = `https://vixsrc.to/embed/movie/${imdbId}`;
     } else if (args.type === "series") {
-        // Correctly handling Season and Episode for Vixsrc
         const season = idParts[1] || "1";
         const episode = idParts[2] || "1";
         streamUrl = `https://vixsrc.to/embed/tv/${imdbId}/${season}/${episode}`;
@@ -23,12 +20,11 @@ builder.defineStreamHandler((args) => {
         return Promise.resolve({
             streams: [{
                 name: "Vixsrc",
-                title: `🔗 Open in Vixsrc (HD)`,
-                externalUrl: streamUrl // Vixsrc works best as an external link
+                title: "📺 Watch it",
+                externalUrl: streamUrl
             }]
         });
     }
-
     return Promise.resolve({ streams: [] });
 });
 
