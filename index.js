e8c8f6bda797ed0c99133ad961261a2e0cf1ac62
alien-homesick <alien.and.homesick@gmail.com>
2025-12-18 16:20:18 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
+const manifest = require("./manifest.json");
+
+const builder = new addonBuilder(manifest);
+
+builder.defineStreamHandler((args) => {
+    const idParts = args.id.split(":");
+    const imdbId = idParts[0];
+    let streamUrl = "";
+
+    if (args.type === "movie") {
+        streamUrl = `https://vixsrc.to/embed/movie/${imdbId}`;
+    } else if (args.type === "series") {
+        const season = idParts[1];
+        const episode = idParts[2];
+        streamUrl = `https://vixsrc.to/embed/tv/${imdbId}/${season}/${episode}`;
+    }
+
+    if (streamUrl) {
+        return Promise.resolve({
+            streams: [{
+                name: "Vixsrc",
+                title: "📺 Watch on Vixsrc (HD)",
+                externalUrl: streamUrl
+            }]
+        });
+    }
+
+    return Promise.resolve({ streams: [] });
+});
+
+module.exports = (req, res) => {
+    builder.getInterface().execute(req, res);
+};
